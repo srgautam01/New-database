558baeee8e6bb0dafb23a3d30dba80bc95365a70
SUndar Raj Gautam <sundar.gautam@selu.edu>
2019-03-04 18:20:30 -0600

    adding css tools

---
+++ src/components/tools/tools.js
@@ -0,0 +1,14 @@
+import React,  {Component} from "react";
+import './tools.css';
+
+class ToolComponent extends Component{
+    render(){
+        return(
+            <div className={"Tool"}>
+                Tool
+            </div>
+        );
+    }
+}
+
+export default ToolComponent;
