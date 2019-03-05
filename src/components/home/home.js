f19a55b1fa1254ec8874dbc035c158751c83ed02
SUNDAR RAJ GAUTAM <sundar.gautam@selu.edu>
2019-03-04 18:15:08 -0600

    Home component made.

---
+++ src/components/home/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './home.css';
+
+class HomeComponent extends Component{
+    render() {
+        return (
+            <div className={"Home"}>
+                Home
+            </div>
+        )
+    }
+}
+
+export default HomeComponent;
