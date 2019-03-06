4ad5667d7552744798a6b824069e4a18e6602c48
SUNDAR RAJ GAUTAM <sundar.gautam@selu.edu>
2019-03-06 17:16:50 -0600

    added link and back to home page

--- src/components/tools/tools.js
+++ src/components/tools/tools.js
@@ -1,11 +1,16 @@
 import React,  {Component} from "react";
 import './tools.css';
+import {Link} from 'react-router-dom';
+
 
 class ToolComponent extends Component{
     render(){
         return(
             <div className={"Tool"}>
-                Tool
+                <h1>I am tool page</h1>
+                <Link to={'/'}>
+I am being pushed back to home.
+                </Link>
             </div>
         );
     }
