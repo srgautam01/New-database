403699a47a20e0bced0cea989acbc4f285e06b45
SUNDAR RAJ GAUTAM <sundar.gautam@selu.edu>
2019-03-06 17:21:23 -0600

    Added about us to navbar

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -12,7 +12,7 @@ class NavBarComponent extends Component {
         return(
 
             <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <Link className="navbar-brand" to="/"></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -27,9 +27,13 @@ class NavBarComponent extends Component {
                         <li className="nav-item">
                             <Link to="/tools" className="nav-link">Tools</Link>
                         </li>
-
+                        <li className="nav-item">
+                            <Link to="/about" className="nav-link">About us</Link>
+                        </li>
 
                     </ul>
+
+
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
