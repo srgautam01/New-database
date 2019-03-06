b4f1276d2821fdec3bd619d1f510ad24663077f8
SUNDAR RAJ GAUTAM <sundar.gautam@selu.edu>
2019-03-06 17:12:14 -0600

    added about component

---
+++ src/components/about/about.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './about.css';
+import {Link} from 'react-router-dom';
+class About extends Component{
+    render() {
+        return (
+            <div className={"About"}>
+                <h1>I am about page</h1>
+            </div>
+        )
+    }
+}
+
+export default About;
