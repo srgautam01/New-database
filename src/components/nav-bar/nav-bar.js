bc6535030c1d6e12a1722f001eccfdb263783589
SUNDAR RAJ GAUTAM <sundar.gautam@selu.edu>
2019-03-18 17:17:37 -0500

    login and search swaped.

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -34,12 +34,12 @@ class NavBarComponent extends Component {
 
                     </ul>
 
-
+                    <Login />
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
-                    <Login />
+
                 </div>
             </nav>
         )
