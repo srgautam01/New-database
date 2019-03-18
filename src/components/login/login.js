588a9fb8b8c73adf45705c47aaa616263f149229
SUNDAR RAJ GAUTAM <sundar.gautam@selu.edu>
2019-03-18 17:17:00 -0500

    login catched when user submits form.

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
 import './login.css';
 import {Link} from 'react-router-dom';
+import { API_LOGIN } from '../../constants'
+
 class Login extends Component{
+    constructor(props){
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+
+        alert('you clickd login!')
+    }
+
     render() {
         return (
             <div className={"login"}>
-                <form className="form-inline my-2 my-lg-0">
+                <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSubmit}>
 
                     <input className="form-control mr-sm-2" type="text" placeholder="Username" aria-label="Username" />
                     <input className="form-control mr-sm-2" type="password" placeholder="Password" aria-label="Password" />
