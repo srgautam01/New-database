f57b11e83710039c377d4157585e97c482180bc3
Chance Stribling <cstribling1717@gmail.com>
2019-05-08 23:39:05 -0500

    removed footer

--- src/App.js
+++ src/App.js
@@ -1,60 +1,59 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBarComponent from './components/nav-bar/nav-bar'
-import HomeComponent from './components/home/home'
-import ToolComponent from './components/tools/tools'
-import FooterComponent from './components/footer/footer'
-import About from './components/about/about'
-import CategoryComponent from './components/category/category'
+import React, { Component } from "react";
+import "./App.css";
+import NavBarComponent from "./components/nav-bar/nav-bar";
+import HomeComponent from "./components/home/home";
+import ToolComponent from "./components/tools/tools";
+import FooterComponent from "./components/footer/footer";
+import About from "./components/about/about";
+import CategoryComponent from "./components/category/category";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import NewHome from './components/new-home-component/new-home-component'
+import NewHome from "./components/new-home-component/new-home-component";
 
 class App extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
-        loginSuccess: false,
-        name: ""
-    }
-    this.handleLogin = this.handleLogin.bind(this)
-}
+      loginSuccess: false,
+      name: ""
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-    handleLogin(event){
-        event.preventDefault();
+  handleLogin(event) {
+    event.preventDefault();
 
-        const username = event.target.elements.username.value;
+    const username = event.target.elements.username.value;
 
-        this.setState({
-            loginSuccess: true,
-            name: username
-        })
+    this.setState({
+      loginSuccess: true,
+      name: username
+    });
 
-        console.log('login requested.');
-    }
+    console.log("login requested.");
+  }
   render() {
-        const loginSuccess = this.state.loginSuccess;
-        const username = this.state.name;
+    const loginSuccess = this.state.loginSuccess;
+    const username = this.state.name;
 
     return (
       <div className="App">
-
         <Router>
-            <div>
-                <NavBarComponent />
+          <div>
+            <NavBarComponent />
 
-                <main className={"main"}>
-                    <Route exact path="/" component={HomeComponent} />
-                    <Route path="/tools" component={ToolComponent} />
-                    <Route path="/about" component={About} />
-                    <Route path="/new" component={NewHome} />
-                    <Route path="/category/:categoryName" component={CategoryComponent} />
-                </main>
-            </div>
+            <main className={"main"}>
+              <Route exact path="/" component={HomeComponent} />
+              <Route path="/tools" component={ToolComponent} />
+              <Route path="/about" component={About} />
+              <Route path="/new" component={NewHome} />
+              <Route
+                path="/category/:categoryName"
+                component={CategoryComponent}
+              />
+            </main>
+          </div>
         </Router>
-          <FooterComponent />
-
-
       </div>
     );
   }
